dc88367d96894dd3bb6810f32541a6ed84a96bee
Baraa <albaraa.m.m2005@gmail.com>
2026-07-02 02:21:21 +0300

    feat: add user and role models, configure Prisma, and set up user service with tests

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { UsersService } from './users/users.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
