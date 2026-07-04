a67fa19b4f835edd9efec97e20ed2074a340df98
Baraa <albaraa.m.m2005@gmail.com>
2026-07-04 20:14:12 +0300

    feat: restructure application by removing AppController and AppService, and adding Auth and Prisma modules with corresponding services and controllers

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
-import { UsersService } from './users/users.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule],
-  controllers: [AppController],
-  providers: [AppService, UsersService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UsersModule,
+    PrismaModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
